df187b6c0ac9bbed77d8141803e04376cd503299
arwaalbathi <109977321+arwaalbathi@users.noreply.github.com>
2022-08-03 19:55:08 +0300

    day3

---
+++ day 2 classwork 1/day 2 classwork 1/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  day 2 classwork 1
+//
+//  Created by Arwa b Albathi on 04/01/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Text("Hello my name is Arwa")
+    .padding(.all,20)
+    .font(.largeTitle)
+            
+        
+    };
+};
+  
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(
+      
+            
+             }
+        }
---
+++ day 2 classwork 1/day 2 classwork 1/day_2_classwork_1App.swift
@@ -0,0 +1,17 @@
+//
+//  day_2_classwork_1App.swift
+//  day 2 classwork 1
+//
+//  Created by Arwa b Albathi on 04/01/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct day_2_classwork_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
